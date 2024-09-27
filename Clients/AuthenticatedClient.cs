42d42c7805e35d7f569b0caa55808cf64b994f01
yoshiioko <acardoza@pdx.edu>
2024-09-27 08:51:52 -0700

    first commit

---
+++ Clients/AuthenticatedClient.cs
@@ -0,0 +1,34 @@
+using GameStore.Client.Models;
+using System.Net.Http.Json;
+
+namespace GameStore.Client.Clients;
+
+public class AuthenticatedClient
+{
+    private readonly HttpClient httpClient;
+
+    public AuthenticatedClient(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task AddGameAsync(Game game)
+    {
+        await httpClient.PostAsJsonAsync("games", game);
+    }
+
+    public async Task<Game> GetGameAsync(int id)
+    {
+        return await httpClient.GetFromJsonAsync<Game>($"games/{id}") ?? throw new Exception("Could not find game!");
+    }
+
+    public async Task UpdateGameAsync(Game updatedGame)
+    {
+        await httpClient.PutAsJsonAsync($"games/{updatedGame.Id}", updatedGame);
+    }
+
+    public async Task DeleteGameAsync(int id)
+    {
+        await httpClient.DeleteAsync($"games/{id}");
+    }
+}
